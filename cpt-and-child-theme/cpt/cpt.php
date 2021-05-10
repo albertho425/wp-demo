4854de5f55acc1d38a8f5c65da32e891defc46bc
maplesyrubweb <73809301+maplesyrupweb@users.noreply.github.com>
2021-05-09 18:20:17 -0700

    Create cpt.php

---
+++ cpt-and-child-theme/cpt/cpt.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Plugin Name:       Post Types and Taxonomies
+ * Plugin URI:        http://github.com/jcasabona/lil-post-types/
+ * Description:       A simple plugin for creating custom post types and taxonomies related to a business directory.
+ * Version:           1.0.0
+ * Author:            Maple Syrup Web
+ * Author URI:        https://maplesyrupweb.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+
+//define the domain
+define( 'MYDOMAIN', 'cpt' );
+
+//define the path
+define( 'MYPATH', plugin_dir_path( __FILE__ ) );
+
+//custom post types
+require_once( MYPATH . '/post-types/register.php' );
+add_action( 'init', 'register_accomodation_type' );
+add_action( 'init', 'register_coffee_shop_type' );
+add_action( 'init', 'register_attractions_type' );
+
+//taxonomies
+require_once( MYPATH . '/taxonomies/register.php' );
+add_action( 'init', 'register_location_taxonomy' );
+?>
