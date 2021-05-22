f3d3be279ee7674742e11d0d7fb6a50a69db7bbe
maplesyrubweb <73809301+maplesyrupweb@users.noreply.github.com>
2021-05-21 23:46:58 -0700

    Initial Commit

---
+++ plugins/remove-css/remove-css.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ *   Plugin Name: Remove Un-needed CSS from your website
+ * 
+ *   Plugin URI: https://maplesyrupweb.com
+ *   Description: Remove Un-needed CSS from your website
+ *   Version: 1.0
+ *   Author: Maple Syrup Web
+ *   Author URI: https://maplesyrupweb.com
+ *   Text Domain: Remove Un-needed CSS from your website
+ *   Domain Path: /lang
+ * 
+ */
+
+    remove_action('wp_print_styles','print_emoji_styles');
+
+
+
+/**
+ *  Remove Block Library CSS
+ */
+function remove_block_library()
+{
+    wp_dequeue_style('wp-block-library');
+    
+
+    
+}
+
+add_action('wp_print_styles', 'remove_block_library');
+
+
+/**
+ *  Remove Dashicons CSS
+ */
+ 
+function remove_dashicons()
+ {
+    wp_dequeue_style('dashicons');
+ }
+
+ add_action('wp_print_styles', 'remove_dashicons'); 
+
+ /**
+ *  Remove Other CSS That you don't need
+ *  When viewing page source, look for link rel="stylesheet" id='name of CSS Here'
+ *  
+ *  
+ */
+
+function remove_travern_theme_css()
+{
+    wp_dequeue_style('travern-gfonts-Assistant');
+    wp_dequeue_style('travern-gfonts-Oleo');
+    wp_dequeue_style('travern-gfonts-Source');
+    wp_dequeue_style('travern-gfonts-Rajdhani');
+    wp_dequeue_style('travern-gfonts-Tangerine');
+    wp_dequeue_style('travern-gfonts-Poppins');
+    wp_dequeue_style('travern-gfonts-DScript');
+    wp_dequeue_style('travern-gfonts-Satisfy');
+    wp_dequeue_style('travern-gfonts-Playfair');
+    wp_dequeue_style('travern-opal-style');
+    wp_dequeue_style('travern-hotel-booking-style');
+}
+
+add_action('wp_print_styles', 'remove_travern_theme_css');
+
+
+
+ ?>
